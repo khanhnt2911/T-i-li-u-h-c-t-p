3c7f40943c106c31f327e26da1abf2c6e855deb6
Nguyễn Tiến Khánh <khanh.nt1@cmctelecom.vn>
2024-06-20 11:08:46 +0700

    add design singleton

---
+++ design-patterns/Singleton/src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Singleton singleton = Singleton.getInstance();
+        System.out.println(singleton);
+    }
+}
