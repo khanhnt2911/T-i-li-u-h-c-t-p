30b2dd539d4688744d97781315d38e93e353f92a
Nguyễn Tiến Khánh <khanh.nt1@cmctelecom.vn>
2024-06-27 14:48:27 +0700

    update design

--- design-patterns/factory_method/src/main/java/org/example/ConcreateCreatorA.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-
-public class ConcreateCreatorA extends Creator {
-    @Override
-    public Product factoryMethod() {
-        return new ProductA();
-    }
-}
--- design-patterns/factory_method/src/main/java/org/example/Main.java
+++ design-patterns/factory_method/src/main/java/org/example/Main.java
@@ -2,8 +2,8 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        Creator creator = new ConcreateCreatorA();
+        Creator creator = new ConcreteCreatorA();
         Product prodA = creator.factoryMethod();
-        creator.someOperation();
+        prodA.doSomething();
     }
 }
